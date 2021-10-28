a2676ee930c7e31a5cb7c4ef9b18859dfd0938da
Rhandy (DiD) <rhandy.grard@gmail.com>
2021-10-28 12:02:45 +0200

    fix(species_sites): Try to reduce species site endpoint complexity

--- backend/gncitizen/core/areas/routes.py
+++ backend/gncitizen/core/areas/routes.py
@@ -669,48 +669,53 @@ def get_species_sites_by_program(id):
             logged_user_id = get_id_role_if_exists()
             if logged_user_id:
                 species_sites_query = (species_sites_query
-                               .outerjoin(AreasAccessModel, AreasAccessModel.id_area == AreaModel.id_area)
-                               .filter(or_(
-                                    SpeciesSiteModel.id_role == logged_user_id,
-                                    AreaModel.id_role == logged_user_id,
-                                    AreasAccessModel.id_user == logged_user_id
-                                ))
+                    .filter(
+                        SpeciesSiteModel.id_role == logged_user_id
+                    )
                 )
+                # species_sites_query = (species_sites_query
+                #                .outerjoin(AreasAccessModel, AreasAccessModel.id_area == AreaModel.id_area)
+                #                .filter(or_(
+                #                     SpeciesSiteModel.id_role == logged_user_id,
+                #                     AreaModel.id_role == logged_user_id,
+                #                     AreasAccessModel.id_user == logged_user_id
+                #                 ))
+                # )
             else:
                 return prepare_list([])
 
         species_sites = (species_sites_query
-             .filter(
-                or_(
-                    SpeciesSiteModel.json_data.comparator.has_key('is_dead') != True,
-                    SpeciesSiteModel.json_data['is_dead'].astext == "false"
-                )
-            )
+            #  .filter(
+            #     or_(
+            #         SpeciesSiteModel.json_data.comparator.has_key('is_dead') != True,
+            #         SpeciesSiteModel.json_data['is_dead'].astext == "false"
+            #     )
+            # )
             .order_by(func.lower(SpeciesSiteModel.name))
             .all()
          )
 
         formatted_list = prepare_list(species_sites, model_name="species_sites")
-
-        for species_site in formatted_list.features:
-            species_site["properties"]["photos"] = []
-            photos = (
-                db.session.query(MediaModel, SpeciesSiteModel)
-                    .filter(SpeciesSiteModel.id_species_site == species_site['properties']['id_species_site'])
-                    .join(
-                        MediaOnSpeciesSiteModel,
-                        MediaOnSpeciesSiteModel.id_data_source == SpeciesSiteModel.id_species_site,
-                    )
-                    .join(MediaModel, MediaOnSpeciesSiteModel.id_media == MediaModel.id_media)
-                    .all()
-            )
-            species_site["properties"]["photos"] = [
-                {
-                    "url": "/media/{}".format(p.MediaModel.filename),
-                    "id_media": p.MediaModel.id_media
-                }
-                for p in photos
-            ]
+        #
+        # for species_site in formatted_list.features:
+        #     species_site["properties"]["photos"] = []
+        #     photos = (
+        #         db.session.query(MediaModel, SpeciesSiteModel)
+        #             .filter(SpeciesSiteModel.id_species_site == species_site['properties']['id_species_site'])
+        #             .join(
+        #                 MediaOnSpeciesSiteModel,
+        #                 MediaOnSpeciesSiteModel.id_data_source == SpeciesSiteModel.id_species_site,
+        #             )
+        #             .join(MediaModel, MediaOnSpeciesSiteModel.id_media == MediaModel.id_media)
+        #             .all()
+        #     )
+        #     species_site["properties"]["photos"] = [
+        #         {
+        #             "url": "/media/{}".format(p.MediaModel.filename),
+        #             "id_media": p.MediaModel.id_media
+        #         }
+        #         for p in photos
+        #     ]
 
         return formatted_list
     except Exception as e:
